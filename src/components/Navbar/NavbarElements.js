c0e8ae2eb9fd3347ff8974e991f81aa8547f9765
ShamSuffescom <sham.suffescom@gmail.com>
2024-03-04 18:10:03 +0530

    fisrt2 commit

--- src/components/Navbar/NavbarElements.js
+++ src/components/Navbar/NavbarElements.js
@@ -63,27 +63,27 @@ white-space: nowrap; */
 // import React from "react";
 // import { Nav, NavLink, NavMenu } from "./NavbarElements";
 
-const Navbar = () => {
-	return (
-		<>
-			<Nav>
-				<NavMenu>
-					<NavLink to="/about" activeStyle>
-						About
-					</NavLink>
-					<NavLink to="/contact" activeStyle>
-						Contact Us
-					</NavLink>
-					<NavLink to="/blogs" activeStyle>
-						Blogs
-					</NavLink>
-					<NavLink to="/sign-up" activeStyle>
-						Sign Up
-					</NavLink>
-				</NavMenu>
-			</Nav>
-		</>
-	);
-};
+// const Navbar = () => {
+// 	return (
+// 		<>
+// 			<Nav>
+// 				<NavMenu>
+// 					<NavLink to="/about" activeStyle>
+// 						About
+// 					</NavLink>
+// 					<NavLink to="/contact" activeStyle>
+// 						Contact Us
+// 					</NavLink>
+// 					<NavLink to="/blogs" activeStyle>
+// 						Blogs
+// 					</NavLink>
+// 					<NavLink to="/sign-up" activeStyle>
+// 						Sign Up
+// 					</NavLink>
+// 				</NavMenu>
+// 			</Nav>
+// 		</>
+// 	);
+// };
 
-export default Navbar;
+// export default Navbar;
